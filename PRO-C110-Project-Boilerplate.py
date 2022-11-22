ca52148a2ad134f521b3eb26ba32511becff11b8
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2022-11-22 09:23:30 -0600

    Add files via upload

--- PRO-C110-Project-Boilerplate.py
+++ PRO-C110-Project-Boilerplate.py
@@ -6,34 +6,39 @@ import numpy as np
 
 
 # import the tensorflow modules and load the model
+import tensorflow as tf
 
+model = tf.keras.model.load_model('keras_model.h5')
 
 
 # Attaching Cam indexed as 0, with the application software
 camera = cv2.VideoCapture(0)
 
 # Infinite loop
-while True:
+	while True:
 
 	# Reading / Requesting a Frame from the Camera 
-	status , frame = camera.read()
+		status , frame = camera.read()
 
 	# if we were sucessfully able to read the frame
-	if status:
+		if status:
 
 		# Flip the frame
-		frame = cv2.flip(frame , 1)
+        	frame = cv2.flip(frame , 1)
 		
 		
 		
 		#resize the frame
-		
+		img = cv2.resize(frame, (224,224))
 		# expand the dimensions
-		
+		test_image = np.array(img, dtype = np.float32)
+		test_image = np.expand_dims(test_image, axis=0)
 		# normalize it before feeding to the model
-		
+		normalising_image = test_image/255.0
 		# get predictions from the model
+		prediction = model.predict(normalising_image)
 		
+    	print("Prediction : ", prediction)
 		
 		
 		# displaying the frames captured
